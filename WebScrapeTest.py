edd634b0f7a86f9afa5b0da6e92ec4e86e881da5
Seamus <63012394+Rennik2@users.noreply.github.com>
2024-07-04 12:03:26 -0700

    start using Botasaurus to asses the menue

--- WebScrapeTest.py
+++ WebScrapeTest.py
@@ -1,18 +1,40 @@
-import re 
+# import requests
 
-from urllib.request import urlopen
+# from urllib.request import urlopen
 
 
-url = "https://www.dreamhorse.com/"
+# url = "https://www.sandomenicocafe.com"
 
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+# page = urlopen(url)
+# html_bytes = page.read()
+# html = html_bytes.decode("utf-8")
 
-print(html)
-# title_index = html.find("<title>")
 
-# start_index = title_index + len("<title>")
-# end_index = html.find("</title>")
+# print(html)
 
-# print(html[start_index:end_index])
+
+
+
+
+from botasaurus.request import request, Request
+from botasaurus.soupify import soupify
+from bs4 import BeautifulSoup
+
+@request
+def scrape_heading_task(request: Request, data):
+    # Visit the Omkar Cloud website
+    response = request.get("https://www.sandomenicocafe.com/")
+
+    # Create a BeautifulSoup object    
+    soup = soupify(response)
+    
+    # Retrieve the heading element's text
+    site = soup.decode("utf-8")
+    doc = BeautifulSoup(site, "html.parser")
+    print(doc.prettify())
+    # Save the data as a JSON file in output/scrape_heading_task.json
+    return {
+        "heading": doc.prettify()
+    }     
+# Initiate the web scraping task
+scrape_heading_task()
