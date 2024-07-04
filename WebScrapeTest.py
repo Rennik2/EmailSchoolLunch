b7521802fc80ac6fd1d503e576254359a20ecde9
Seamus <63012394+Rennik2@users.noreply.github.com>
2024-07-04 14:31:01 -0700

    can now get the lunch table

--- WebScrapeTest.py
+++ WebScrapeTest.py
@@ -2,21 +2,21 @@ from botasaurus.browser import browser, Driver
 from botasaurus.soupify import soupify
 from bs4 import BeautifulSoup
 
-from botasaurus.browser import browser, Driver
 
 @browser
 def scrape_lunch_table_task(driver: Driver, data):
     driver.google_get("https://www.sandomenicocafe.com/")
 
     html = (BeautifulSoup(driver.page_html, "html.parser")).prettify()
+    table_start = html.find('id="elementor-tab-content-1271"')
+    table_end = html[table_start:].find('</div>') + table_start
 
-
-
-    print(html)
+    print(html[table_start:table_end])
+    print(table_start, table_end)
 
     # Save the data as a JSON file in output/scrape_lunch_table_task.json
     return {
-        "html": html
+        "html": html[table_start:table_end]
     }     
 
 # Initiate the web scraping task
