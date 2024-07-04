cafec23a0d3dca3621f9302a1b0d3dc466918d94
Seamus <63012394+Rennik2@users.noreply.github.com>
2024-07-04 12:25:59 -0700

    gets all the html

--- WebScrapeTest.py
+++ WebScrapeTest.py
@@ -1,40 +1,23 @@
-# import requests
-
-# from urllib.request import urlopen
-
-
-# url = "https://www.sandomenicocafe.com"
-
-# page = urlopen(url)
-# html_bytes = page.read()
-# html = html_bytes.decode("utf-8")
-
-
-# print(html)
-
-
-
-
-
-from botasaurus.request import request, Request
+from botasaurus.browser import browser, Driver
 from botasaurus.soupify import soupify
 from bs4 import BeautifulSoup
 
-@request
-def scrape_heading_task(request: Request, data):
-    # Visit the Omkar Cloud website
-    response = request.get("https://www.sandomenicocafe.com/")
+from botasaurus.browser import browser, Driver
 
-    # Create a BeautifulSoup object    
-    soup = soupify(response)
-    
-    # Retrieve the heading element's text
-    site = soup.decode("utf-8")
-    doc = BeautifulSoup(site, "html.parser")
-    print(doc.prettify())
-    # Save the data as a JSON file in output/scrape_heading_task.json
+@browser
+def scrape_lunch_table_task(driver: Driver, data):
+    driver.google_get("https://www.sandomenicocafe.com/")
+
+    html = (BeautifulSoup(driver.page_html, "html.parser")).prettify()
+
+
+
+    print(html)
+
+    # Save the data as a JSON file in output/scrape_lunch_table_task.json
     return {
-        "heading": doc.prettify()
+        "html": html
     }     
+
 # Initiate the web scraping task
-scrape_heading_task()
+scrape_lunch_table_task()
